813e035dfc0208deb7f7ebc5801d726ae8e4fc10
AndreaBz03 <balzamo.1987012@studenti.uniroma1.it>
2024-10-22 18:48:24 +0200

    team.php updated

--- team/team.php
+++ team/team.php
@@ -9,58 +9,22 @@ $dbusername = "root";
 $password = "";
 $dbname = "gamerstats";
 $username = 'prova';
-//debug
+//debug, the session should already exist when logged in
 session_start();
-$user_id=1;
-$_SESSION['id'] = $user_id;
+$user_nickname=1;
+$_SESSION['nickname'] = $user_nickname;
 
 $conn = new mysqli($servername, $dbusername, $password, $dbname);
-function generateUniqueId($mysqli) {
-    $id = uniqid("TM", false);
-    $query = "SELECT * FROM teams WHERE id = '$id'";
-    $result = $mysqli->query($query);
-    if ($result->num_rows > 0) {
-      return generateUniqueId($mysqli);
-    } else {
-      return $id;
-    }
-  }
   
 
-if (isset($_POST['teamName'])) {
+if (isset($_POST['teamName']) && isset($_POST['game'])) {
     $teamName = $_POST['teamName'];
-
-    if (isset($_SESSION['id'])) {
-        $uniqueId = generateUniqueId($conn);
-        $stmt = $conn->prepare("INSERT INTO teams (id, team_name) VALUES (?, ?)");
-        $stmt->bind_param("ss", $uniqueId, $teamName);
-        if ($stmt->execute()) {
-            echo "Team row inserted successfully";
-        } else {
-            echo "Error: " . $stmt->error;
-        }    
-        // Create the team table if it does not exist
-        $sql = "CREATE TABLE IF NOT EXISTS $uniqueId (
-        member_id INT(11) AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
-        leader INT NOT NULL
-        )";
-        if ($conn->query($sql) !== TRUE) {
-            echo "Error creating table: " . $conn->error;
-        }
-
-        $stmt = $conn->prepare("INSERT INTO $uniqueId (name, leader) VALUES (?, ?)");
-        $leaderId = (int) $_SESSION['id'];
-        $isLeader = 1;
-        $stmt->bind_param("si", $leaderId, $isLeader); //string and integer
-        if ($stmt->execute()) {
-            echo "Team created successfully";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
+    $gameName = $_POST['game'];
+    $stmt = $conn->prepare("INSERT INTO teams (team_name, game, member1, member2, member3, member4, member5, member6, leader) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $teamName, $gameName, $user_nickname, $member1_nickname, $member2_nickname, $member3_nickname, $member4_nickname, $member5_nickname, $user_nickname);
+    if ($stmt->execute()) {
+        echo "Team row inserted successfully";
     } else {
-        echo "Error: Session ID not set";
-    }
-    
-
+        echo "Error: " . $stmt->error;
+    }    
 }
